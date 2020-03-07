899ff6a23a4a623da13a952fd0492a38f5ab0896
Anay Wadhera <awadhera@berkeley.edu>
2020-03-07 00:23:54 -0800

    Settings: allow for customizing the length of the navigation handle [2/2]
    Change-Id: I6002824fe781557ebff4263d2dde117ff425ae2a
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
+++ src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
@@ -43,8 +43,10 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
     private static final String TAG = "GestureNavigationBackSensitivityDialog";
     private static final String KEY_BACK_SENSITIVITY = "back_sensitivity";
     private static final String KEY_BACK_HEIGHT = "back_height";
+    private static final String KEY_HOME_HANDLE_SIZE = "home_handle_width";
 
-    public static void show(SystemNavigationGestureSettings parent, int sensitivity, int height) {
+    public static void show(SystemNavigationGestureSettings parent, int sensitivity, int height, int length) {
+
         if (!parent.isAdded()) {
             return;
         }
@@ -54,6 +56,7 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
         final Bundle bundle = new Bundle();
         bundle.putInt(KEY_BACK_SENSITIVITY, sensitivity);
         bundle.putInt(KEY_BACK_HEIGHT, height);
+        bundle.putInt(KEY_HOME_HANDLE_SIZE, length);
         dialog.setArguments(bundle);
         dialog.setTargetFragment(parent, 0);
         dialog.show(parent.getFragmentManager(), TAG);
@@ -82,6 +85,8 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                 mHapticSwitchChecked = hapticSwitch.isChecked() ? true : false;
             }
         });
+        final SeekBar seekBarHandleSize = view.findViewById(R.id.home_handle_seekbar);
+        seekBarHandleSize.setProgress(getArguments().getInt(KEY_HOME_HANDLE_SIZE));
         final Switch arrowSwitch = view.findViewById(R.id.back_arrow_gesture_switch);
         mArrowSwitchChecked = Settings.Secure.getInt(getActivity().getContentResolver(),
                 Settings.Secure.SHOW_BACK_ARROW_GESTURE, 1) == 1;
@@ -111,11 +116,14 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                     getArguments().putInt(KEY_BACK_SENSITIVITY, sensitivity);
                     int height = seekBarHeight.getProgress();
                     getArguments().putInt(KEY_BACK_HEIGHT, height);
+                    int length = seekBarHandleSize.getProgress();
+                    getArguments().putInt(KEY_HOME_HANDLE_SIZE, length);
                     SystemNavigationGestureSettings.setBackHeight(getActivity(), height);
                     SystemNavigationGestureSettings.setBackSensitivity(getActivity(),
                             getOverlayManager(), sensitivity);
                     Settings.System.putInt(getContext().getContentResolver(),
                             Settings.System.BACK_GESTURE_HAPTIC, mHapticSwitchChecked ? 1 : 0);
+                    SystemNavigationGestureSettings.setHomeHandleSize(getActivity(), length);
                     Settings.Secure.putInt(getActivity().getContentResolver(),
                             Settings.Secure.SHOW_BACK_ARROW_GESTURE, mArrowSwitchChecked ? 1 : 0);
                     Settings.System.putInt(getActivity().getContentResolver(),
