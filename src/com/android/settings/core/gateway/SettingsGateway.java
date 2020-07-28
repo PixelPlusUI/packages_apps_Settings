Havoc-OS/android_packages_apps_Settings

Settings: Integrate ConfigCenter into Settings
Based on both DU's Nougat and Omni's Oreo implimentations, so
credits to mazda and maxwen for this.

@mydongistiny: fixed up some for dashboard/android10

Havoc icon by @SKULSHADY

Change-Id: Id4e5367bab5826bfc8bced5684fc2e599f8140c4
Signed-off-by: Joe Maples <joe@frap129.org>
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -149,6 +149,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.havoc.config.center.ConfigCenter;
+
 public class SettingsGateway {
 
     /**
@@ -285,7 +287,8 @@ public class SettingsGateway {
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
-            LiveDisplaySettings.class.getName()
+            LiveDisplaySettings.class.getName(),
+            ConfigCenter.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -324,5 +327,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.ConfigCenterActivity.class.getName(),
     };
 }

PixelPlusUI/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -148,6 +148,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.ppui.config.center.ConfigCenter;
+
 public class SettingsGateway {
 
     /**
@@ -283,7 +285,8 @@ public class SettingsGateway {
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
-            DarkModeSettingsFragment.class.getName()
+            DarkModeSettingsFragment.class.getName(),
+            ConfigCenter.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -322,5 +325,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.ConfigCenterActivity.class.getName(),
     };
 }