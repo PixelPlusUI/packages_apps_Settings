BlissRoms/platform_packages_apps_Settings

Settings: Initial import for Pixel-specific feature Settings
Change-Id: If9a87439e03be388de0e5b9059dc93c77243062d
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

Android.mk
@@ -22,6 +22,7 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../Blissify/src) 
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../external/google/SettingsGoogle/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx-constraintlayout_constraintlayout \
@@ -56,7 +57,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    packages/apps/Blissify/res
+    packages/apps/Blissify/res \
+    external/google/SettingsGoogle/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 

PixelPlusUI/packages_apps_Settings

Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

Android.mk
@@ -22,6 +22,7 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../ConfigCenter/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../external/google/SettingsGoogle/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx-constraintlayout_constraintlayout \
@@ -55,7 +56,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     packages/apps/TouchGestures/res \
-    packages/apps/ConfigCenter/res
+    packages/apps/ConfigCenter/res \
+    external/google/SettingsGoogle/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 